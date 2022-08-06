4d31e3b05b8e14f641cf56f2343133d43b4fd633
HCXXYBBD <103500453+HCXXYBBD@users.noreply.github.com>
2022-08-06 21:48:00 +0800

    Add files via upload

---
+++ 8_6/8_6.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+// typedef struct 
+// {
+//     char name[20];
+//     char id[9];
+//     int age;
+//     char tel[11];
+// }Stu;
+
+// struct
+// {
+//     char name[20];
+//     char id[9];
+//     int age;
+//     char tel[11];
+// }s;
+
+
+
+// int main()
+// {
+//     // Stu s={"张三","213200554",19,"12345678911"};
+//     // struct s;
+//     s={"张三","213200554",19,"12345678911"};
+//     printf("学生:>%s,%s,%d,%s",s.name,s.id,s.age,s.tel);
+//     return 0;
+// }
